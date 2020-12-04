ac0326996a107527dad52a8c451b220203125e97
gommorin <luis.gomezmorin@gmail.com>
2020-12-04 10:26:51 -0600

    adding tournaments section

--- front/neku-react-app/src/App.js
+++ front/neku-react-app/src/App.js
@@ -2,16 +2,18 @@
 import './App.css';
 
 // COMPONENTS //
-import NavBar from './components/NavBar'
-import Header from './components/Header'
-import News from './components/News'
+// import NavBar from './components/NavBar'
+// import Header from './components/Header'
+// import News from './components/News'
+import Tournaments from './components/Tournaments'
 
 function App() {
   return (
     <div>
-      <NavBar />
+      {/* <NavBar />
       <Header />
-      <News />
+      <News /> */}
+      <Tournaments />
     </div>
   );
 }
