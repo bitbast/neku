c2a5887e66015efee324b685b952a1599536fc96
luisbastida <luisbastida86@gmail.com>
2020-12-04 22:25:18 -0600

    Teams page

--- front/neku-react-app/src/components/profileTourCard/index.js
+++ front/neku-react-app/src/components/profileTourCard/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import { Container, Row, Col, Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
+import { Row, Col, Card, CardImg, CardBody } from "reactstrap";
 import 'bootstrap/dist/css/bootstrap.css';
 import './profileTourCard.css'
 
--- front/neku-react-app/src/pages/TeamsPlayers/index.js
+++ front/neku-react-app/src/pages/TeamsPlayers/index.js
@@ -1,13 +1,13 @@
 import React, {Component} from "react"
-import { Container, Row, Col, Card, CardImg, CardBody} from "reactstrap";
+import { Container, Row, Col} from "reactstrap";
 import 'bootstrap/dist/css/bootstrap.css';
-import "./TeamsPlayers.css";
+import "./TeamsPlayers.css"
 
 class TeamsPlayers extends Component {
     render() {
         return(
-            <Container fluid className="profileContainer">
-                <Row className="d-flex justify-content-center align-items-center playerNameBox">
+            <Container fluid className="teamsContainer">
+                <Row className="d-flex justify-content-center align-items-center sectionNameBox">
                     <Col xs="12" md="6" className= "d-flex justify-content-center align-items-center">
                         <p className= "playerName">Equipos!</p>
                     </Col>
@@ -15,27 +15,60 @@ class TeamsPlayers extends Component {
                             <img max width="100%" src="https://www.techgames.com.mx/wp-content/uploads/2019/08/Telcel-LVP-Mexico-equipos.jpg" alt="Gamer img" />
                     </Col> 
                 </Row>
-                <Row className="overflow-auto">
-                    <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg="4" >
+                <Row >
+                    <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg="4" className=" mb-3 mt-3 colTeam" >
                     <a href="/Profile">
-                        <Card className="TournamentCard m-3">
-                            <CardImg top width="100%" height="150" src="https://a.espncdn.com/photo/2020/0115/r652568_888x500_16-9.png" alt="Tournament" className="TournametImg" />
-                        </Card>
-                        </a>
+                        <div className="p-2 TournamentCard">
+                            <img width="100%" height="200" src="https://a.espncdn.com/photo/2020/0115/r652568_888x500_16-9.png" alt="Tournament" className="teamImage" />
+                            <p className=" teamName text-center text-white">Equipo: Anahuac</p>
+                            <img width="50" height="50" src="https://picsum.photos/200" alt="pais flag" className="rounded-circle mx-auto d-block"/>
+                        </div>
+                    </a>
                     </Col>
-                    <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg="4" >
-                        <Card className="TournamentCard m-3">
-                            <CardImg top width="100%" height="150" src="https://a.espncdn.com/photo/2020/0115/r652568_888x500_16-9.png" alt="Tournament" className="TournametImg" />
-                        </Card>
-                    </Col> <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg="4" >
-                        <Card className="TournamentCard m-3">
-                            <CardImg top width="100%" height="150" src="https://a.espncdn.com/photo/2020/0115/r652568_888x500_16-9.png" alt="Tournament" className="TournametImg" />
-                        </Card>
+                    <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg="4" className=" mb-3 mt-3 colTeam" >
+                    <a href="/Profile">
+                        <div className="p-2 TournamentCard">
+                            <img width="100%" height="200" src="https://a.espncdn.com/photo/2020/0115/r652568_888x500_16-9.png" alt="Tournament" className="teamImage" />
+                            <p className=" teamName text-center text-white">Equipo: Anahuac</p>
+                            <img width="50" height="50" src="https://picsum.photos/200" alt="pais flag" className="rounded-circle mx-auto d-block"/>
+                        </div>
+                    </a>
                     </Col>
-                    <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg="4" >
-                        <Card className="TournamentCard m-3">
-                            <CardImg top width="100%" height="150" src="https://a.espncdn.com/photo/2020/0115/r652568_888x500_16-9.png" alt="Tournament" className="TournametImg" />
-                        </Card>
+                    <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg="4" className=" mb-3 mt-3 colTeam" >
+                    <a href="/Profile">
+                        <div className="p-2 TournamentCard">
+                            <img width="100%" height="200" src="https://a.espncdn.com/photo/2020/0115/r652568_888x500_16-9.png" alt="Tournament" className="teamImage" />
+                            <p className=" teamName text-center text-white">Equipo: Anahuac</p>
+                            <img width="50" height="50" src="https://picsum.photos/200" alt="pais flag" className="rounded-circle mx-auto d-block"/>
+                        </div>
+                    </a>
+                    </Col>
+                    <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg="4" className=" mb-3 mt-3 colTeam" >
+                    <a href="/Profile">
+                        <div className="p-2 TournamentCard">
+                            <img width="100%" height="200" src="https://a.espncdn.com/photo/2020/0115/r652568_888x500_16-9.png" alt="Tournament" className="teamImage" />
+                            <p className=" teamName text-center text-white">Equipo: Anahuac</p>
+                            <img width="50" height="50" src="https://picsum.photos/200" alt="pais flag" className="rounded-circle mx-auto d-block"/>
+                        </div>
+                    </a>
+                    </Col>
+                    <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg="4" className=" mb-3 mt-3 colTeam p-1" >
+                    <a href="/Profile">
+                        <div className="p-1 TournamentCard">
+                            <img width="100%" height="200" src="https://a.espncdn.com/photo/2020/0115/r652568_888x500_16-9.png" alt="Tournament" className="teamImage" />
+                            <p className=" teamName text-center text-white">Equipo: Anahuac</p>
+                            <img width="50" height="50" src="https://picsum.photos/200" alt="pais flag" className="rounded-circle mx-auto d-block"/>
+                        </div>
+                    </a>
+                    </Col>
+                    <Col xs={{size: 10, offset: 1}} md={{size: 6, offset: 0}} lg="4" className=" mb-3 mt-3 colTeam" >
+                    <a href="/Profile">
+                        <div className="p-2 TournamentCard">
+                            <img width="100%" height="200" src="https://a.espncdn.com/photo/2020/0115/r652568_888x500_16-9.png" alt="Tournament" className="teamImage" />
+                            <p className=" teamName text-center text-white">Equipo: Anahuac</p>
+                            <img width="50" height="50" src="https://picsum.photos/200" alt="pais flag" className="rounded-circle mx-auto d-block"/>
+                        </div>
+                    </a>
                     </Col>
                 </Row>
             </Container>
