ac1670555a6781fc684ca35792c0144885ddbe64
gommorin <luis.gomezmorin@gmail.com>
2020-12-05 18:28:44 -0600

    correcciones

--- front/neku-react-app/src/pages/Videogames/index.js
+++ front/neku-react-app/src/pages/Videogames/index.js
@@ -9,8 +9,9 @@ class Videogames extends Component {
         return(
             <Container fluid className="teamsContainer">
                 <Row className="d-flex justify-content-center align-items-center sectionNameBox">
-                    <Col xs="12" md="6" className= "d-flex justify-content-center align-items-center">
-                        <p className= "playerName">Juegos!</p>
+                <Col xs="12" md="6" className= "heading">
+                        <h1>JUEGOS</h1>
+                        <h4>La razón de ser de un gamer</h4>
                     </Col>
                     <Col xs="12" md="4" className="d-flex justify-content-center align-items-center">
                             <img max width="100%" height="250" src="https://assets.eleconomista.com.mx/__export/1581119523386/sites/eleconomista/img/2020/02/07/que-son-esports.jpg_554688468.jpg" alt="Gamer img" />
