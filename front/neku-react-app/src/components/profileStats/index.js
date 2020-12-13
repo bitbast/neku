274f8f179737e0a3a71650d1b62c4c405e659391
luisbastida <luisbastida86@gmail.com>
2020-12-12 21:23:49 -0600

    Connection of profileStats

--- front/neku-react-app/src/components/profileStats/index.js
+++ front/neku-react-app/src/components/profileStats/index.js
@@ -23,45 +23,46 @@ const ProfileStats = () => {
         setPlayer(playerData.data.team)
     }
 
+
     return (
         <Row className="mt-2 ">
             <Col md="12" className="sectionTitle">Estadísticas</Col>
             <Col md="4" className="d-flex flex-column justify-content-center align-items center" >
                 <p className="profileStatsTitle text-center mb-2">Total de Partidas</p>
-                <h3 className="profileStatsData text-center mb-5"> {player.name} </h3>
+                <h3 className="profileStatsData text-center mb-5"> {player.totalMatches}</h3>
             </Col>
             <Col md="4" className="d-flex flex-column justify-content-center align-items center">
                 <p className="profileStatsTitle text-center mb-2">% Ganados</p>
-                <h3 className="profileStatsData text-center mb-5">75%</h3>
+                <h3 className="profileStatsData text-center mb-5"> {player.winPercentage}%</h3>
             </Col>
             <Col md="4" className="d-flex flex-column justify-content-center align-items center">
                 <p className="profileStatsTitle text-center mb-2">Ranking</p>
-                <h3 className="profileRankingData text-center mb-5">1</h3>
+                <h3 className="profileRankingData text-center mb-5">{player.ranking}</h3>
             </Col>
             <Col md="4" className="d-flex flex-column justify-content-center align-items center">
                 <p className="profileStatsTitle text-center mb-2">Equipo</p>
-                <img width="100" height="100" src="" alt="Equipo logo" className="rounded-circle mx-auto d-block mb-5" />
+                <img width="100" height="100" src={player.teamLogo} alt="Equipo logo" className="rounded-circle mx-auto d-block mb-5" />
 
             </Col>
             <Col md="4" className="d-flex flex-column justify-content-center align-items center">
                 <p className="profileStatsTitle text-center mb-2 align-self-center">País</p>
-                <img width="100" height="100" src="https://picsum.photos/200" alt="pais flag" className="rounded-circle mx-auto d-block mb-5"/>
+                <img width="100" height="100" src={player.countryLogo} alt="pais flag" className="rounded-circle mx-auto d-block mb-5"/>
             </Col>
             <Col md="4" className="d-flex flex-column  align-items center">
                 <p className="profileStatsTitle text-center mb-2">Hrs de juego</p>
-                <h3 className="profileStatsData text-center mb-5">300</h3>
+                <h3 className="profileStatsData text-center mb-5">{player.gameTime} </h3>
             </Col>
             <Col md="4" className="d-flex flex-column justify-content-center align-items center">
-                <p className="profileStatsTitle text-center mb-2">KD</p>
-                <h3 className="profileStatsData text-center mb-5">84%</h3>
-            </Col>
+                <p className="profileStatsTitle text-center mb-2">Torneos jugados</p>
+                <h3 className="profileStatsData text-center mb-5">{player.gamesPlayed[0].game1.totalEvents}</h3>
+            </Col>{}
             <Col md="4" className="d-flex flex-column justify-content-center align-items center">
                 <p className="profileStatsTitle text-center mb-2">Score</p>
-                <h3 className="profileStatsData text-center mb-5">3000</h3>
+                <h3 className="profileStatsData text-center mb-5">{player.gamesPlayed[0].game1.score}</h3>
             </Col>
             <Col md="4" className="d-flex flex-column justify-content-center align-items center">
                 <p className="profileStatsTitle text-center mb-2">Tiempo promedio</p>
-                <h3 className="profileStatsData text-center mb-5">4</h3>
+                <h3 className="profileStatsData text-center mb-5">{player.gamesPlayed[0].game1.avgMatchTime}</h3>
             </Col>
         </Row>
 
